adec54dd55780f738641ead26fbf768510b73397
abdalazez <abdalazzezzalsalahat@icloud.com>
2021-06-06 14:22:09 +0200

    basic files, last commit on t=the main

---
+++ main.py
@@ -0,0 +1,11 @@
+import face_recognition
+import cv2
+from face_recognition.api import face_encodings
+import numpy as np
+import re 
+import json as j
+import datetime
+
+date_time = datetime.datetime.now()
+pics = []
+
