be18e7365517b6aab08e7e8e058c9d480adc5bfb
abdalazez <abdalazzezzalsalahat@icloud.com>
2021-06-07 09:46:17 +0200

    function calls fixes

--- main.py
+++ main.py
@@ -36,16 +36,16 @@ def access_cam():
     cap.release()
     cv2.destroyAllWindows()
 
-
+access_cam()
 # frame from the cv
-face_from_cam = face_recognition.load_image_file('./assets/noor.jpg')
+face_from_cam = face_recognition.load_image_file(cap_frame_name)
 face_from_cam_encodings = face_recognition.face_encodings(face_from_cam)[0]
 
 def employee(index):
     """
     A function that takes an index and return the employee info
     """
-    print("It's a picture of me!")
+    # print("It's a picture of me!")
     info = json_data[index]
     send_email(info, date_time)
 
@@ -61,13 +61,13 @@ def send_email(info = None, d_time = '9:00'):
         print(f'''      Dear esteemed security department, 
         Our system detected {calling}. {info["Name"]} from {info["Department"]} department
         {gend} job id {info["Job_Id"]} who is not wearing a mask at {d_time}, 
-        please check attached photo below.(Screenshot)''')
+        please check attached photo below.{cap_frame_name}''')
         ### the correct email validation and connection
 
     else: 
         print(f'''      Dear esteemed security department, 
     Our system detected a visitor who is not wearing a mask at {d_time}, 
-    please check attached photo below.(Screenshot)''')
+    please check attached photo below.{cap_frame_name}''')
         ### the correct email validation and connection
 
 def recognize_face(known_face_encodings):
@@ -80,11 +80,13 @@ def recognize_face(known_face_encodings):
         results = face_recognition.compare_faces([face], face_from_cam_encodings)
         name  = 'unknown'
         if results[0] == True:
-            print("it is an employee")
+            employee(cout)
+            # print("it is an employee")
             break
         cout += 1
     if results[0] == False:
-        print ("not employee")
+        # print ("not employee")
+        send_email(None, date_time)
         # for none employers code. take a screen shot and send an e-mail
 
 ### possibly in recognize_face function 
@@ -102,6 +104,7 @@ with open('employees.json', 'r') as jd:
     json_data = j.load(jd)
     for p in json_data:
         pics.append(p["photos"])
+    encode_known_pics(pics)
     
 def detect_mask():
     """
@@ -113,6 +116,6 @@ def red_alert():
     pass
 
 
-if __name__ == "__main__":
-    encode_known_pics(pics)
-    access_cam()
+# if __name__ == "__main__":
+#     encode_known_pics(pics)
+#     access_cam()
